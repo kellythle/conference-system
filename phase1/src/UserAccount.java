7497b4cb3fb8ce016ca0a69f4d12422df37bad1b
chaolinwang <cl.wang@mail.utoronto.ca>
2020-10-25 14:09:35 -0700

    Moved entities to src

--- phase1/src/UserAccount.java
+++ phase1/src/UserAccount.java
@@ -1,12 +1,12 @@
-
+import java.util.ArrayList;
 abstract class UserAccount {
     private String userName;
     private String password;
-    private ArrayList<Stirng> friendList; // stores the userNames
+    private ArrayList<String> friendList; // stores the userNames
     private ArrayList<Integer> messageSent;
     private ArrayList<Integer> messageRecieved;
 
     //**TODO getters setters constructors
-    public boolean isOrganizer() {};
-    public boolean isSpeaker() {};
+    public abstract boolean isOrganizer() ;
+    public abstract boolean isSpeaker() ;
 }
