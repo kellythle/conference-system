7b156b32791b28b6ccf767dbc94f47b9e2d89ba0
yenan <an.yen@mail.utoronto.ca>
2020-10-26 00:26:14 -0400

    An Yen completed entity class Conference.java and Event .java with constructors, getters, setters, and Javadoc.

--- phase1/src/Conference.java
+++ phase1/src/Conference.java
@@ -1,4 +1,52 @@
+import java.util.ArrayList;
+/**
+ * A class that represents a conference.
+ * @author An Yen
+ */
 class Conference {
     private Arraylist<Stirng> attendees;
-    private Arralist<Integer> events;
+    private Arraylist<Integer> events;
+
+    /**
+     * Creates a Conference with an attendee list and an event list.
+     * @param attendees
+     * @param events
+     */
+    public Conference(ArratList<String> attendees, ArrayList<Integer> events){
+        this.attendees = attendees;
+        this.events = events;
+    }
+
+    /**
+     * Returns the event list of this conference.
+     * @return the event list of thi Conference
+     */
+    public Arraylist<Integer> getEvents() {
+        return events;
+    }
+
+    /**
+     * Returns the attendee list of this conference.
+     * @return the attendee list of this Conference
+     */
+    public Arraylist<Stirng> getAttendees() {
+        return attendees;
+    }
+
+    /**
+     * Add a new attendee to the attendee list of this conference.
+     * @param attendeeName
+     */
+    public void setAttendees(String attendeeName){
+        attendees.add(attendeeName);
+    }
+
+    /**
+     * Add a new event to the event list of this conference.
+     * @param eventId
+     */
+    public void setEvents (int eventId){
+        events.add(eventId);
+    }
+
 }
--- phase1/src/Event.java
+++ phase1/src/Event.java
@@ -1,8 +1,119 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * A class that represents an event.
+ * @author An Yen
+ */
 class Event {
     private String name; // name of event
-    private int Id;
+    private int Id; // id of event
     private String speaker; // name of speaker
-    private Time time;
-    private Arraylist<String> attendees; // usernames
-    private String room;
+    private Date time; //time of the event
+    private Arraylist<String> attendees; // names of the attendees
+    private String room; //the room number where this event is held
+
+    /**
+     * The name, ID, occuring time, occuring place, and speaker required to create an event.
+     * @param name
+     * @param id
+     * @param speaker
+     * @param time
+     * @param room
+     */
+    public Event (String name, int id, String speaker, Date time, String room) {
+        this.name = name;
+        this.id = id;
+        this.speaker = speaker;
+        this.time = time;
+        this.room = room;
+    }
+
+    /**
+     * Return the name of this event.
+     * @return the name of this Event
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     Return the ID of this event.
+     * @return the id of this Event.
+     */
+    public int getId() {
+        return Id;
+    }
+
+    /**
+     Return the speaker's name of this event.
+     * @return the Speaker's name of this Event
+     */
+    public String getSpeaker() {
+        return speaker;
+    }
+
+    /**
+     * Returns the occuring time of this event.
+     * @return the occuring time of this Event
+     */
+    public Date getTime() {
+        return time;
+    }
+
+    /**
+     * Returns the occuring room number of this event.
+     * @return the occuring room number of this Event
+     */
+    public String getRoom() {
+        return room;
+    }
+
+    /**
+     * This method sets the attendee list of this event.
+     * @param attendees
+     */
+    public void setAttendees(Arraylist<String> attendees) {
+        this.attendees = attendees;
+    }
+
+    /**
+     * This method sets the ID of this event.
+     * @param id
+     */
+    public void setId(int id) {
+        Id = id;
+    }
+
+    /**
+     * This method sets the name of this event.
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * This method sets the room number of where this event will be held.
+     * @param room
+     */
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    /**
+     * This method assign a speaker to this event by the speaker's name.
+     * @param speaker
+     */
+    public void setSpeaker(String speaker) {
+        this.speaker = speaker;
+    }
+
+    /**
+     * This method sets the occuring time of this event.
+     * @param time
+     */
+    public void setTime(Date time) {
+        this.time = time;
+    }
 }
