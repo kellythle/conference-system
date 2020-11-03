14f10cb39066e0c975e956fe1c7cc86fd70a4701
yenan <an.yen@mail.utoronto.ca>
2020-11-03 17:56:22 -0500

    An Yen modifies class Conference. Changed variable legal rooms to an arraylist of Pair<room number, room capacity>. All other related methods are changed.

--- phase1/src/Conference.java
+++ phase1/src/Conference.java
@@ -1,3 +1,5 @@
+import javafx.util.Pair;
+
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Date;
@@ -8,33 +10,33 @@ import java.util.Map;
  * The conference stores the earliest legal starting time and the
  * last legal ending time for events of a day.
  * The conference also stores a map, Map<Date, Integer>, that represents
- * the scheduled times and room numbers.
+ * the scheduled times and rooms.
  *
  * @author An Yen
  */
 class Conference {
     private int legalStartHour = 9; //earliest legal starting time for events of a day
     private int legalEndHour = 17; //last legal ending time for events of a day
-    private ArrayList<Integer> legalRoomNumbers; // a list of existing room numbers
-    private Map<Date, Integer> scheduledSpot; // a map of time to room number that has been scheduled
+    private ArrayList<Pair<Integer, Integer>> legalRooms; // a list of existing rooms
+    private Map<Date, Pair<Integer, Integer>> scheduledSpot; // a map of time to room number that has been scheduled
 
     /**
-     * Creates a conference with the list of room numbers.
+     * Creates a conference with the list of rooms.
      * This conference has default start hour and end hour. (from 9am to 5 pm)
-     * @param RoomNumbers- an Arraylist of room numbers
+     * @param Rooms- an Arraylist of Pairs<Integer roomNumber, Integer roomCapacity>
      */
-    public Conference(ArrayList<Integer> RoomNumbers){
-        this.legalRoomNumbers = RoomNumbers;
+    public Conference(ArrayList<Pair<Integer, Integer>> Rooms){
+        this.legalRooms = Rooms;
     }
 
     /**
-     * Creates a conference by the given start hour, end hour and a list of room numbers.
+     * Creates a conference by the given start hour, end hour and a list of rooms.
      * @param startHour- earliest legal starting time for events of a day
      * @param endHour- last legal ending time for events of a day
-     * @param RoomNumbers- a list of existing room numbers
+     * @param Rooms- a list of existing rooms
      */
-    public Conference(int startHour, int endHour, ArrayList<Integer> RoomNumbers){
-        this(RoomNumbers);
+    public Conference(int startHour, int endHour, ArrayList<Pair<Integer, Integer>> Rooms){
+        this(Rooms);
         this.legalStartHour = startHour;
         this.legalEndHour = endHour;
     }
@@ -59,25 +61,25 @@ class Conference {
      * Returns a map of scheduled spots tha maps from time to room number.
      * @return a Map of the scheduled spots.
      */
-    public Map<Date, Integer> getScheduledSpot() {
+    public Map<Date, Pair<Integer, Integer>> getScheduledSpot() {
         return scheduledSpot;
     }
 
     /**
      * Returns a list of legal room numbers.
-     * @return an Arraylist of legal room numbers.
+     * @return an Arraylist of Pairs<Integer roomNumber, Integer roomCapacity>
      */
-    public ArrayList<Integer> getLegalRoomNumbers() {
-        return legalRoomNumbers;
+    public ArrayList<Pair<Integer, Integer>> getLegalRooms() {
+        return legalRooms;
     }
 
     /**
      * Add a new scheduled spot to the list of scheduled spots.
      * @param date- occurring time of the event
-     * @param roomNumber- occurring room number of the event
+     * @param room- occurring room of the event
      */
-    public void addScheduledSpot(Date date, int roomNumber){
-        this.scheduledSpot.putIfAbsent(date, roomNumber);
+    public void addScheduledSpot(Date date, Pair<Integer, Integer> room){
+        this.scheduledSpot.putIfAbsent(date, room);
     }
 
 }
