91fc3e99dc3d06d2d296f9319fd843067591b5b3
yenan <an.yen@mail.utoronto.ca>
2020-11-05 23:35:14 -0500

    An Yen completes the first draft of SignUpController. One method needs to be modified after merging StorageManager and UserManager.

--- phase1/src/Controller/SignUpController.java
+++ phase1/src/Controller/SignUpController.java
@@ -2,16 +2,27 @@ package Controller;
 
 import Entity.Event;
 import UseCase.EventManager;
+import UseCase.UserManagaer;
 
 import java.util.ArrayList;
 
 public class SignUpController {
     private EventManager eventManager;
+    private UserManagaer userManagaer;
     /**
      * Returns an arraylist of the existing events.
      * @return an ArrayList<Event> of the existing events
      */
     public ArrayList<Event> getEventList(){
         return eventManager.getEventList();
+    }
+
+    public boolean SignUpEvent(String username, Event event){
+        if(eventManager.addUserToEvent(username, event)){
+            //Remove the comment notation after merging Storage and UserManager
+            //userManager.addRegisteredEvent(username, event.getName());
+            return true;
+        }
+        return false;
     }
 }
