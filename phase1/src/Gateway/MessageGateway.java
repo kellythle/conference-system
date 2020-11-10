aa258eee3d150b252404490c394233e1a669b6b6
Filip Jovanovic <filip.jovanovic@mail.utoronto.ca>
2020-11-10 15:58:20 -0500

    Implemented reading from / writing to files for UserGateway and MessageGateway, some TODOs in order for code to work.

--- phase1/src/Gateway/MessageGateway.java
+++ phase1/src/Gateway/MessageGateway.java
@@ -1,4 +1,49 @@
 package Gateway;
 
+import Entity.Message;
+import UseCase.MessageManager;
+
+import java.io.*;
+import java.util.HashMap;
+
 public class MessageGateway {
+
+    private MessageManager messageManager;
+
+    public void saveToFile(String writeFilepath) throws IOException
+    {
+        OutputStream file = new FileOutputStream(writeFilepath);
+        OutputStream buffer = new BufferedOutputStream(file);
+        ObjectOutput output = new ObjectOutputStream(buffer);
+
+        // Get HashMap from UserManager
+        // TODO: Need getter / setter for MessageManager systemMessages
+        // HashMap<Integer, Message> messageHashMap = messageManager.systemMessages;
+
+        // Write to file and close streams
+        // output.writeObject(messageHashMap);
+        output.close();
+        buffer.close();
+        file.close();
+    }
+
+    public void readFromFile(String readFilepath) throws ClassNotFoundException
+    {
+        try {
+            InputStream file = new FileInputStream(readFilepath); // String path should be "fileName.ser"
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input = new ObjectInputStream(buffer);
+
+            // deserialize the HashMap
+            // TODO: Need getter / setter for MessageManager systemMessages
+            // messageManager.systemMessages = (HashMap) input.readObject();
+            input.close();
+            buffer.close();
+            file.close();
+
+        } catch (IOException ex) {
+            // TODO: Need getter / setter for MessageManager systemMessages
+            // messageManager.systemMessages = new HashMap<Integer, Message>();
+        }
+    }
 }
--- phase1/src/Gateway/UserGateway.java
+++ phase1/src/Gateway/UserGateway.java
@@ -1,4 +1,46 @@
 package Gateway;
 
-public class UserGateway {
+import Entity.UserAccount;
+import UseCase.UserManager;
+import java.io.*;
+import java.util.HashMap;
+
+public class UserGateway implements ReadWrite {
+
+    private UserManager userManager;
+
+    public void saveToFile(String writeFilepath) throws IOException
+    {
+        OutputStream file = new FileOutputStream(writeFilepath);
+        OutputStream buffer = new BufferedOutputStream(file);
+        ObjectOutput output = new ObjectOutputStream(buffer);
+
+        // Get HashMap from UserManager
+        HashMap<String, UserAccount> userHashMap = userManager.userMap;
+
+        // Write to file and close streams
+        output.writeObject(userHashMap);
+        output.close();
+        buffer.close();
+        file.close();
+    }
+
+    public void readFromFile(String readFilepath) throws ClassNotFoundException
+    {
+        try {
+            InputStream file = new FileInputStream(readFilepath); // String path should be "fileName.ser"
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input = new ObjectInputStream(buffer);
+
+            // deserialize the HashMap
+            userManager.userMap = (HashMap) input.readObject();
+            input.close();
+            buffer.close();
+            file.close();
+
+        } catch (IOException ex) {
+            userManager.userMap = new HashMap<String, UserAccount>();
+        }
+    }
 }
+
