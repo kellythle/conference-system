f23effb5275341a68d503bb3e66640c8cb5a8fc6
Filip Jovanovic <filip.jovanovic@mail.utoronto.ca>
2020-11-09 23:39:06 -0500

    Fixed package for ReadWrite

--- phase1/src/Gateway/ReadWrite.java
+++ phase1/src/Gateway/ReadWrite.java
@@ -1,10 +1,10 @@
-package Controller;
+package Gateway;
 
 import java.io.IOException;
 
 public interface ReadWrite {
 
-    void saveToFile(String writefilepath) throws IOException;
+    void saveToFile(String writeFilepath) throws IOException;
 
     <T> Class<T> readFromFile(String readFilepath, Class<T> container) throws ClassNotFoundException;
 
