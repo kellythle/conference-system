bc3006fb2e7de90a3bcc40875851515d07bca01a
chaolinwang <cl.wang@mail.utoronto.ca>
2020-11-10 12:27:28 -0800

    Updated the functionality of interface ReadWrite after discussion with Filip

--- phase1/src/Gateway/ReadWrite.java
+++ phase1/src/Gateway/ReadWrite.java
@@ -1,11 +1,16 @@
 package Gateway;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+/**
+ * Interface for Gateways classes making sure they can read/write
+ *
+ * @author Filip Jovanovic, Chaolin Wang
+ */
 public interface ReadWrite {
-
+    //Gets/accesses a container from a use case and serializes to the given file path
     void saveToFile(String writeFilepath) throws IOException;
-
-    <T> Class<T> readFromFile(String readFilepath, Class<T> container) throws ClassNotFoundException;
-
+    //Reads a container from writeFilepath and stores it to the respective use case that has it
+    void readFromFile(String readFilepath) throws ClassNotFoundException;
 }
