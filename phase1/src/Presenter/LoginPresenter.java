9b4866be769e78e240ddd478ea756741ff4b334e
Qianwen Wu <richardqianwen.wu@mail.utoronto.ca>
2020-11-11 20:05:25 -0500

    Current version of login presenter (Without LoginController set as parameter)

--- phase1/src/Presenter/LoginPresenter.java
+++ phase1/src/Presenter/LoginPresenter.java
@@ -1,4 +1,40 @@
 package Presenter;
 
+
 public class LoginPresenter {
+
+    /**
+     * display information when an account is created
+     * @param value- boolean value got from login controller
+     */
+    public void displayAccountCreateInfo(boolean value){
+        if (value){
+            System.out.println("The user account is created successfully");
+        }
+        else
+            System.out.println("The user account is existed");
+    }
+
+    /**
+     * display information when a user is logging in
+     * @param value- boolean value got from login controller
+     */
+    public void displayLoginInfo(boolean value){
+
+        if (value){
+            System.out.println("Correct Password");
+        }
+        else {
+            System.out.println("Incorrect user name or password");
+        }
+    }
+
+    /**
+     * display information about current login user
+     * @param info- user name got from login controller
+     */
+    public void displayLoginUser(String info){
+        System.out.println("Current user is "+ info);
+    }
 }
+
