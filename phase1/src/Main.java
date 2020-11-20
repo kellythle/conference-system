9b07d5d3378b0f87f9102b1b157197f422b62c20
Kelly Le <56463950+Rikuuro@users.noreply.github.com>
2020-11-20 13:17:00 -0500

    Added javadoc to Main

--- phase1/src/Main.java
+++ phase1/src/Main.java
@@ -1,5 +1,8 @@
 import Controller.ConferenceSystem;
 
+/**
+ * A class that starts a conference.
+ */
 public class Main {
     public static void main(String[] args) {
         ConferenceSystem cs = new ConferenceSystem();
