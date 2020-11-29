7ed62ad2b3d648e94d750748515b2bed167264bb
chaolinwang <cl.wang@mail.utoronto.ca>
2020-11-29 13:00:28 -0800

    Added more functionality to VIP, including its own main menu, message menu, and such. Still needs a lot more testing.

---
+++ phase2/src/Controller/VIPMessageController.java
@@ -0,0 +1,52 @@
+package Controller;
+
+import UseCase.MessageManager;
+import UseCase.UserManager;
+
+import java.util.Scanner;
+
+public class VIPMessageController extends MessageController{
+
+    /**
+     * Constructor for VIPMessageController
+     * Creates a new instance of messagePresenter.
+     *
+     * @param username       String username of current user
+     * @param myUserManager  instance of UserManager
+     * @param messageManager instance of MessageManager
+     */
+    public VIPMessageController(String username, UserManager myUserManager, MessageManager messageManager) {
+        super(username, myUserManager, messageManager);
+    }
+
+    /**
+     * Calls MessagePresenter to print VIP message menu. Prompts for a response to a list of menu options.
+     *
+     * @return the String input from the User.
+     */
+    public String getVIPMessageMenu() {
+        Scanner scan = new Scanner(System.in);
+        messagePresenter.printVIPMessageMenu();
+        return scan.nextLine();
+    }
+
+    /**
+     * Prompts user for receiver ID and message content. Sends single message.
+     */
+    @Override
+    public void sendMessage(){
+        messagePresenter.printAttendees(myUserManager);
+        messagePresenter.printSpeakers(myUserManager);
+        messagePresenter.printVIPs(myUserManager);
+        Scanner scanner = new Scanner(System.in);
+        messagePresenter.printReceiverIDPrompt();
+        String receiver = scanner.nextLine();
+        messagePresenter.printContentPrompt();
+        String content = scanner.nextLine();
+        if (sendSingleMessage(receiver, content)){
+            messagePresenter.printMessageSuccess();
+        } else {
+            messagePresenter.printMessageFailed();
+        }
+    }
+}
--- phase2/src/UseCase/VIPFactory.java
+++ phase2/src/UseCase/VIPFactory.java
@@ -12,8 +12,7 @@ public class VIPFactory implements AccountFactory {
      *
      * @param userName The new user's username, *cannot be a duplicate within the system
      * @param password The new user's password
-     * @return The newly created Attendee
-     * @throws IllegalArgumentException If there is a duplicate userName
+     * @return The newly created VIP
      */
     @Override
     public UserAccount createAccount(String userName, String password) {
