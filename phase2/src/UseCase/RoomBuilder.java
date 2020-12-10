c634b50a5c2d2a6b18a0f4bfff3506dc8c1b7b26
Filip Jovanovic <filipjovanovic1160@gmail.com>
2020-12-09 20:17:26 -0500

    Added missing Javadocs and fixed some warnings.

--- phase2/src/UseCase/RoomBuilder.java
+++ phase2/src/UseCase/RoomBuilder.java
@@ -2,13 +2,7 @@ package UseCase;
 
 import Entity.Room;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.TreeMap;
-
 public class RoomBuilder {
-    private TreeMap<LocalDateTime, LocalDateTime> schedule = new TreeMap<>();
     private int roomNumber;
     private int capacity;
     private int squareFootage;
@@ -20,56 +14,110 @@ public class RoomBuilder {
     private String specialFeatures;
     private String description;
 
+    /**
+     * Sets this RoomBuilder's roomNumber and returns the RoomBuilder.
+     * @param roomNumber the room number
+     * @return the RoomBuilder
+     */
     public RoomBuilder roomNumber(int roomNumber) {
         this.roomNumber = roomNumber;
         return this;
     }
 
+    /**
+     * Sets this RoomBuilder's capacity and returns the RoomBuilder.
+     * @param capacity the room capacity
+     * @return the RoomBuilder
+     */
     public RoomBuilder capacity(int capacity) {
         this.capacity = capacity;
         return this;
     }
 
+    /**
+     * Sets this RoomBuilder's square footage and returns the RoomBuilder.
+     * @param squareFootage the room square footage
+     * @return the RoomBuilder
+     */
     public RoomBuilder squareFootage(int squareFootage) {
         this.squareFootage = squareFootage;
         return this;
     }
 
+    /**
+     * Sets this RoomBuilder's number of screens and returns the RoomBuilder.
+     * @param screens the room number of screens
+     * @return the RoomBuilder
+     */
     public RoomBuilder screens(int screens) {
         this.screens = screens;
         return this;
     }
 
+    /**
+     * Sets if this RoomBuilder has a sound system and returns the RoomBuilder.
+     * @param soundSystem if the room has a sound system or not
+     * @return the RoomBuilder
+     */
     public RoomBuilder soundSystem(boolean soundSystem) {
         this.soundSystem = soundSystem;
         return this;
     }
 
+    /**
+     * Sets if this RoomBuilder has a stage and returns the RoomBuilder.
+     * @param stage if the room has a stage or not
+     * @return the RoomBuilder
+     */
     public RoomBuilder stage(boolean stage) {
         this.stage = stage;
         return this;
     }
 
+    /**
+     * Sets if this RoomBuilder is accessible for people with disabilities and returns the RoomBuilder.
+     * @param accessible if the room is accessible
+     * @return the RoomBuilder
+     */
     public RoomBuilder accessible(boolean accessible) {
         this.accessible = accessible;
         return this;
     }
 
+    /**
+     * Sets if this RoomBuilder has WiFi and returns the RoomBuilder.
+     * @param wifi if the room has Wifi or not
+     * @return the RoomBuilder
+     */
     public RoomBuilder wifi(boolean wifi) {
         this.wifi = wifi;
         return this;
     }
 
+    /**
+     * Sets this RoomBuilder's special features and returns the RoomBuilder.
+     * @param specialFeatures the room special features
+     * @return the RoomBuilder
+     */
     public RoomBuilder specialFeatures(String specialFeatures) {
         this.specialFeatures = specialFeatures;
         return this;
     }
 
+    /**
+     * Sets this RoomBuilder's description and returns the RoomBuilder.
+     * @param description the room description
+     * @return the RoomBuilder
+     */
     public RoomBuilder description(String description) {
         this.description = description;
         return this;
     }
 
+    /**
+     * Creates the new Room based on the RoomBuilder's variable values and returns the Room.
+     * @return the new Room
+     */
     public Room build() {
         Room room = new Room();
 
